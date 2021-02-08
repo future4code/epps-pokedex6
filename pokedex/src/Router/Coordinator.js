b8025b22c10f7b259031c8ee87e64309d75c3934
ArturNicolaiewskyPellicioli <arturpellicioli@hotmail.com>
2021-02-08 14:52:44 -0300

    configurando e criando as rotas

---
+++ pokedex/src/Router/Coordinator.js
@@ -0,0 +1,12 @@
+
+export const goToPokedex = (history) => {
+    history.push("/pokedex")
+}
+
+export const goToHome = (history) => {
+    history.push("/")
+}
+
+export const goToDetails = (history) => {
+    history.push("/details")
+}
